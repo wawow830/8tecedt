4adb0ab93f850c3a9d94b018f40f82cd94ee7d2e
wawow <wawow830@gmail.com>
2026-07-03 11:41:41 +1000

    shirl

---
+++ 9/calculator.py
@@ -0,0 +1,12 @@
+num1 = float(input("num1: "))
+num2 = float(input("num2: "))
+operation = input("operation: ")
+
+if operation == "add":
+    print(num1 + num2)
+elif operation == "subtract":
+    print(num1 - num2)
+elif operation == "mult":
+    print(num1 * num2)
+elif operation == "divide":
+    print(num1 / num2)
---
+++ 9/mult.py
@@ -0,0 +1,3 @@
+num1 = int(input("num1: "))
+num2 = int(input("num2: "))
+print(num1 * num2)
