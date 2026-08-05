6390a54e7169c0baab1e45d43c8136692cda39f3
wawow <wawow830@gmail.com>
2026-08-05 17:26:55 +1000

    act4e

--- IO/act4.py
+++ IO/act4.py
@@ -9,3 +9,13 @@ with open(fname, "r") as file:
 print(f"Favourites count: {len(favs)}")
 print(f"First item: {favs[0]}")
 print(f"Last item: {favs[-1]}")
+
+print()
+
+print(*(f"{i}. {j}" for i, j in enumerate(favs, start=1)), sep="\n")
+
+query = input("Enter an item to search for: ").title()
+if query in favs:
+    print(f"{query} was found in the file.")
+else:
+    print(f"{query} was not found in the file.")
