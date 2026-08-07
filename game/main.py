b08f3f4ea5ff0cae3c557783a13be11cb26be2cc
wawow <wawow830@gmail.com>
2026-08-07 13:47:36 +1000

    shop

--- game/main.py
+++ game/main.py
@@ -1,16 +1,26 @@
 bits = 0
 bpt = 0
+items = []
 
 shop = {
     "biter" {
-        "cost" = 8,
-        "bpt" = 1
+        "cost": 8,
+        "bpt": 1
     }
 }
+
+def buy(item):
+    if item in shop:
+        if bits >= shop[item]["cost"]:
+            bits -= shop[item]["cost"]
+            items.append(shop[item])
 
 while True:
     if input() == "":
         bits += 1
 
     print("\n" * 80)
+    print("Shop: ")
+    for name, item in shop:
+        print(f"{name}: {item["cost"]}")
     print(f"bits: {bits}")
