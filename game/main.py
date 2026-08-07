5c7da4a237d30d50953c32cf5976ee66391c4811
wawow <wawow830@gmail.com>
2026-08-07 13:43:30 +1000

    guh

---
+++ game/main.py
@@ -0,0 +1,16 @@
+bits = 0
+bpt = 0
+
+shop = {
+    "biter" {
+        "cost" = 8,
+        "bpt" = 1
+    }
+}
+
+while True:
+    if input() == "":
+        bits += 1
+
+    print("\n" * 80)
+    print(f"bits: {bits}")
