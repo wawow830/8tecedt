7edb13e3b8c45acd71999fd5409afb30f2afdadc
wawow <wawow830@gmail.com>
2026-08-07 13:32:22 +1000

    remove keyboard

--- game/main.py
+++ game/main.py
@@ -3,6 +3,7 @@ bps = 0
 
 items = []
 available = []
+selected = 0
 
 shop = {
     "cursor": {
@@ -12,6 +13,7 @@ shop = {
 }
 
 def getAvailable():
+    global bits
     return [
         name
         for name, item in shop.items()
@@ -31,23 +33,25 @@ def buy(item):
         items.append(shop[item])
         computeBps()
 
-def click():
-    global bits
-    if input() == "":
-        bits += 1
-
 def update():
     global bits, available
     bits += bps
     available = getAvailable()
 
 def render():
+    global available, bits
     print("\n" * 80)
     print(f"Shop: {available}")
     print(f"Bits: {bits}")
 
+def click():
+    global bits
+    bits += 1
+
+def select(x):
+    global selected
+    selected += x
 
 while True:
-    click()
     update()
     render()
