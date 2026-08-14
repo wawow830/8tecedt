1cf8382077724b96a41db1d9830bff4ab79581b9
wawow <wawow830@gmail.com>
2026-08-14 11:28:35 +1000

    actually working

--- probs/1.py
+++ probs/1.py
@@ -1,3 +1,3 @@
 tempC = float(input("Temperature in Celsius: "))
 tempF = tempC * 9/5 + 32
-print(f"The temperature in Fahrenheit is 212.0 degrees.")
+print(f"The temperature in Fahrenheit is {tempF} degrees.")
