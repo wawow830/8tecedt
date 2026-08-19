86c9216ded51794528b83a201d5d06a72e4540c9
wawow <wawow830@gmail.com>
2026-08-19 11:25:53 +1000

    act8

---
+++ acts/7.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you? "))
+
+if age < 18:
+    print("You are a child.")
+elif age < 60:
+    print("You are an adult.")
+else:
+    print("You are a pensioner.")
---
+++ acts/8.py
@@ -0,0 +1,6 @@
+start = 1
+end = 10
+
+for i in range(start, end+1):
+    if i % 2 == 0:
+        print(i)
